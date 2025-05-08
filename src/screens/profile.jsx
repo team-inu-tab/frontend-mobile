04f3e1e1c2f7420025f491a2e224571e9276b278
kyeongeunx <gyeongeh@gmail.com>
2025-05-08 16:49:58 +0900

    fix: navigate url

--- src/screens/profile.jsx
+++ src/screens/profile.jsx
@@ -10,7 +10,7 @@ const ProfileScreen = () => {
   const handleButtonClick = async () => {
     try {
       await logout();
-      navigate("/login");
+      navigate("/");
     } catch (err) {
       console.error("로그아웃 실패:", err);
       toast.error("로그아웃 중 문제가 발생했습니다. 다시 시도해주세요.");
