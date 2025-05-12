ac736a56d9d3ff7a7843299f8ec116ddaabaf882
Choseongyul <tjdduf0504@naver.com>
2025-05-12 15:39:47 +0900

    fix: Mail write page

--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,6 @@ createRoot(document.getElementById("root")).render(
     <RouterProvider router={router} />
     <Toaster position="top-center" reverseOrder={false} />
     {/* <MailContainer/> */}
-    {/* <MailWriteModal/> */}
+    <MailWriteModal/>
   </StrictMode>
 );
